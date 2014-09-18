6b72238a4ae8b4de946f645dc902070c1a75cc43
James Shore <jshore@jamesshore.com>
2014-09-18 12:30:54 -0700

    Got basic directives working

--- src/client/example.js
+++ src/client/example.js
@@ -4,8 +4,44 @@
 var module = angular.module("helloWorld", []);
 module.controller("ExampleController", ["$scope", function ($scope) {
 
-	$scope.constructed = function() {
-		return $scope.first + ": " + $scope.second
-	}
+	$scope.startingBalance="start";
 
 }]);
+
+module.directive("stockMarketRow", function() {
+	return {
+		restrict: "A",
+		transclude: false,
+		scope: {},
+		controller: function($scope, $element) {
+
+		},
+		template:
+			'<td>2010</td>' +
+      '<td>{{startingBalance}}</td>' +
+      '<td>$7,000</td>' +
+			'<td class="negative">($695)</td>' +
+			'<td class="negative">($232)</td>' +
+			'<td>$907</td>' +
+      '<td>$9,981</td>'
+	}
+});
+
+module.directive("configurationField", function() {
+
+	return {
+		restrict: "E",
+		transclude: true,
+		scope: {},
+		controller: function($scope, $element) {
+
+		},
+		template:
+			'<div class="config">' +
+			' <label ng-transclude></label>' +
+			' <input type="text" class="invalid" title="Tooltip">' +
+			'</div>',
+		replace: true
+	};
+
+});
