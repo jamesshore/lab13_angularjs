5d37fac5d715bc98ebedba468f6319b62616e9bf
James Shore <jshore@jamesshore.com>
2014-09-17 19:09:11 -0700

    Initial commit (modified from lab10_react)

---
+++ Jakefile.js
@@ -0,0 +1,127 @@
+// Copyright (c) 2012-2014 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+/*global desc, task, jake, fail, complete, directory, require, console, process */
+(function () {
+	"use strict";
+
+	// Uncomment and modify the following list to cause the build to fail unless these browsers are tested.
+	var REQUIRED_BROWSERS = [
+//		"IE 8.0.0 (Windows 7)",
+//		"IE 9.0.0 (Windows 7)",
+//		"Firefox 31.0.0 (Mac OS X 10.8)",
+//		"Chrome 37.0.2062 (Mac OS X 10.8.5)",
+//		"Safari 6.1.6 (Mac OS X 10.8.5)",
+//		"Mobile Safari 7.0.0 (iOS 7.1)"
+	];
+
+	var jshint = require("simplebuild-jshint");
+
+	var GENERATED_DIR = "generated";
+	var CLIENT_DIR = "src/client";
+
+	desc("Delete generated files");
+	task("clean", function() {
+		jake.rmRf(GENERATED_DIR);
+	});
+
+	desc("Lint, test, and build");
+	task("default", [ "lint" ], function() {
+		console.log("\n\nOK");
+	});
+
+	desc("Start HTTP server for manual testing");
+	task("run", function() {
+		jake.exec("node ./node_modules/http-server/bin/http-server src/client", { interactive: true }, complete);
+	}, {async: true});
+
+	desc("Lint everything");
+	task("lint", [ "lintNode", "lintClientJs" ]);
+
+	task("lintNode", function() {
+		process.stdout.write("Linting Node.js code: ");
+		jshint.checkFiles({
+			files: [ "Jakefile.js", "src/server/**/*.js", "build/util/**/*.js" ],
+			options: nodeLintOptions(),
+			globals: nodeLintGlobals()
+		}, complete, fail);
+	}, { async: true });
+
+	task("lintClientJs", function() {
+		process.stdout.write("Linting client-side JavaScript code: ");
+		jshint.checkFiles({
+			files: [ CLIENT_DIR + "/**/*.js", "!" + CLIENT_DIR + "/vendor/**/*" ],
+			options: clientLintOptions(),
+			globals: clientLintGlobals()
+		}, complete, fail);
+	}, { async: true });
+
+
+	function clientJsFiles() {
+		return new jake.FileList(CLIENT_DIR + "/**/*.js").toArray();
+	}
+
+	function globalLintOptions() {
+		return {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: false,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			globalstrict: true,     // "global" stricts are okay when using CommonJS modules
+			trailing: true
+		};
+	}
+
+	function nodeLintOptions() {
+		var options = globalLintOptions();
+		options.node = true;
+		return options;
+	}
+
+	function clientLintOptions() {
+		var options = globalLintOptions();
+		options.browser = true;
+		options.newcap = false;
+		return options;
+	}
+
+	function globalLintGlobals() {
+		return {
+			// Mocha
+			beforeEach: false,
+			afterEach: false,
+			describe: false,
+			it: false,
+
+			// Expect
+			expect: false
+		};
+	}
+
+	function nodeLintGlobals() {
+		return globalLintGlobals();
+	}
+
+	function clientLintGlobals() {
+		var globals = globalLintGlobals();
+
+		// Karma
+		globals.console = false;
+		globals.dump = false;
+
+		// CommonJS
+		globals.exports = false;
+		globals.require = false;
+		globals.module = false;
+
+		return globals;
+	}
+
+}());
---
+++ readme.md
@@ -0,0 +1,39 @@
+The Lab: Front-End Frameworks: AngularJS
+===========
+
+This repository contains the sample code for the forthcoming Front-End Frameworks: AngularJS episodes of James Shore's screencast. Let's Code: Test-Driven JavaScript is a screencast series focused on rigorous, professional JavaScript development.
+
+This is a work in progress.
+
+
+Setup
+-----
+
+To try this code on your own computer:
+
+1. Install [Node.js](http://nodejs.org/download/).
+2. Download and unzip the source code into a convenient directory.
+3. All commands must run from the root of the source tree: `cd <directory>`.
+
+
+Running the Sample Application
+------------------------------
+
+1. Run `./jake.sh run` (Unix/Mac) or `jake run` (Windows)
+2. Open `http://localhost:8080` in a browser.
+
+
+Running the Tests
+-----------------
+
+(Note: tests aren't set up yet, so ignore this section for now. But you can lint by running step 3.)
+
+1. Run `./jake.sh karma` (Unix/Mac) or `jake karma` (Windows) to start the Karma server.
+2. Start the browsers you want to test and point each one at `http://localhost:9876`.
+3. Run `./jake.sh` (Unix/Mac) or `jake` (Windows) every time you want to build and test.
+
+
+License
+-------
+
+MIT License. See `LICENSE.TXT`.
