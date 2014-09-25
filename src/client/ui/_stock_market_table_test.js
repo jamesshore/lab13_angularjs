e1c422b64e18a3b8ba577c5e039c1ba3364d3510
James Shore <jshore@jamesshore.com>
2014-09-25 10:54:37 -0700

    Got ready to test StockMarketTable

--- src/client/ui/_stock_market_table_test.js
+++ src/client/ui/_stock_market_table_test.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2014 Titanium I.T. LLC. All rights reserved. For license, see "README" or "LICENSE" file.
 "use strict";
 
-describe("Nothing", function() {
+describe("StockMarketTable", function() {
 
-	it("tests nothing", function() {
+	it("does nothing", function() {
 		expect(true).to.equal(true);
 	});
 
