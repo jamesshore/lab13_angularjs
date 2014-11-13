7f466a7b2b4e77b41ab50bb3844baf6ec704d5f8
James Shore <jshore@jamesshore.com>
2014-11-13 12:36:05 -0800

    Upgrade Angular 1.3.0-rc2 --> 1.3.2

--- build/config/karma.conf.js
+++ build/config/karma.conf.js
@@ -14,8 +14,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-	    'src/vendor/angular-1.3.0-rc.2.js',
-	    'src/vendor/angular-mocks-1.3.0-rc.2.js',
+	    'src/vendor/angular-1.3.2.js',
+	    'src/vendor/angular-mocks-1.3.2.js',    // angular-mocks MUST be loaded after angular
       'src/client/**/*.js'
     ],
 
